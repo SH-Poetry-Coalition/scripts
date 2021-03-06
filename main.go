a91120fa32ae43e52e3aa78526b0fcbbbbaeafe7
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-03-06 16:01:26 +0800

    chore: 参数个数的判断，增加鲁棒性

--- main.go
+++ main.go
@@ -7,20 +7,23 @@ import (
 )
 
 func main() {
-	// util.Archive()
-	switch os.Args[1] {
-	case "archive":
-		poems := util.ReadPoems("./poems_original")
-		util.Archive("./poems_original", "./poems", poems)
-	case "anonymize":
-		poems := util.ReadPoems("./poems_original")
-		correspondence, reverseCorrespondence := util.GenCorrespondence("./poems_original", poems)
-		util.WriteCorrespondence(correspondence, reverseCorrespondence)
-		util.Anonymize("./poems_original", "./poems_anonymous", correspondence)
-	case "reverse":
-		reverseCorrespondence := util.ReadCorrespondence("reverse_correspondence.txt")
-		util.Reverse("./poems_anonymous", "./poems_reversed", reverseCorrespondence)
-	default:
-		fmt.Println("Expected 'archive' or 'anonymize' subcommands!")
+	if len(os.Args) == 1 {
+		fmt.Println("Expected 'archive' or 'anonymize' or 'reverse' subcommands!")
+	} else {
+		switch os.Args[1] {
+		case "archive":
+			poems := util.ReadPoems("./poems_original")
+			util.Archive("./poems_original", "./poems", poems)
+		case "anonymize":
+			poems := util.ReadPoems("./poems_original")
+			correspondence, reverseCorrespondence := util.GenCorrespondence("./poems_original", poems)
+			util.WriteCorrespondence(correspondence, reverseCorrespondence)
+			util.Anonymize("./poems_original", "./poems_anonymous", correspondence)
+		case "reverse":
+			reverseCorrespondence := util.ReadCorrespondence("reverse_correspondence.txt")
+			util.Reverse("./poems_anonymous", "./poems_reversed", reverseCorrespondence)
+		default:
+			fmt.Println("Expected 'archive' or 'anonymize' or 'reverse' subcommands!")
+		}
 	}
 }
